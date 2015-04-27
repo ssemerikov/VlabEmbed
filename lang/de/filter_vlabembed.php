c3d122c894bddff3806a1f215529dfe85a0f557b
ssemerikov <semerikov@gmail.com>
2015-04-27 17:50:50 +0300

    Update filter_vlabembed.php

--- lang/de/filter_vlabembed.php
+++ lang/de/filter_vlabembed.php
@@ -20,5 +20,19 @@ $string['vlabembed_width'] = 'Die Weite des Applets VLab';
 $string['vlabembed_height'] = 'Die Höhe des Applets VLab';
 $string['vlabembed_lang'] = 'Sprache Applet Vlab';
 $string['vlab_copyright'] = 'IrYdium Projekt\'s Virtual Lab ist ein Projekt der ChemCollective an der Carnegie Mellon';
+$string['vlabembed_langcode_EN'] = 'Englisch';                 
+$string['vlabembed_langcode_AR'] = 'Arabisch';                 
+$string['vlabembed_langcode_CA'] = 'Katalanisch';                  
+$string['vlabembed_langcode_ES'] = 'Spanisch';                 
+$string['vlabembed_langcode_GL'] = 'Galizischen';              
+$string['vlabembed_langcode_LT'] = 'Litauisch';              
+$string['vlabembed_langcode_BR'] = 'Portugiesisch (Brasilien)'; 
+$string['vlabembed_langcode_ZH'] = 'Chinese';                  
+$string['vlabembed_langcode_DE'] = 'Deutsch';                 
+$string['vlabembed_langcode_FR'] = 'Französisch';              
+$string['vlabembed_langcode_GR'] = 'Griechisch';               
+$string['vlabembed_langcode_RU'] = 'Russisch';                  
+$string['vlabembed_langcode_UK'] = 'Ukrainisch';             
 
 ?>
+
