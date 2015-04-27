b09b4223a57de3a504dead6755b3e7070bf8be6b
ssemerikov <semerikov@gmail.com>
2015-04-27 17:50:31 +0300

    Update filtersettings.php

--- filtersettings.php
+++ filtersettings.php
@@ -21,7 +21,24 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
   $settings->add(new admin_setting_configtext('filter_vlabembed_width', get_string('vlabembed_width','filter_vlabembed'), '', '800', PARAM_TEXT));
   $settings->add(new admin_setting_configtext('filter_vlabembed_height', get_string('vlabembed_height','filter_vlabembed'), '', '600', PARAM_TEXT));
-  $settings->add(new admin_setting_configtext('filter_vlabembed_lang', get_string('vlabembed_lang','filter_vlabembed'), '', 'UK'));
+//  $settings->add(new admin_setting_configtext('filter_vlabembed_lang', get_string('vlabembed_lang','filter_vlabembed'), '', 'UK'));
+  $languages = array(
+                    'EN' => get_string('vlabembed_langcode_EN','filter_vlabembed'),  
+                    'AR' => get_string('vlabembed_langcode_AR','filter_vlabembed'),  
+                    'CA' => get_string('vlabembed_langcode_CA','filter_vlabembed'),  
+                    'ES' => get_string('vlabembed_langcode_ES','filter_vlabembed'),  
+                    'GL' => get_string('vlabembed_langcode_GL','filter_vlabembed'),  
+                    'LT' => get_string('vlabembed_langcode_LT','filter_vlabembed'),  
+                    'BR' => get_string('vlabembed_langcode_BR','filter_vlabembed'),  
+                    'ZH' => get_string('vlabembed_langcode_ZH','filter_vlabembed'),  
+                    'DE' => get_string('vlabembed_langcode_DE','filter_vlabembed'),  
+                    'FR' => get_string('vlabembed_langcode_FR','filter_vlabembed'),  
+                    'GR' => get_string('vlabembed_langcode_GR','filter_vlabembed'),  
+                    'RU' => get_string('vlabembed_langcode_RU','filter_vlabembed'),  
+                    'UK' => get_string('vlabembed_langcode_UK','filter_vlabembed')
+                    );
+
+  $settings->add(new admin_setting_configselect('filter_vlabembed_lang', get_string('vlabembed_lang','filter_vlabembed'), '', 'EN', $languages));
 }
 
 ?>
